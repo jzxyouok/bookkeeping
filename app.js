66f9f297ae9b476baf0c2ff1f55e3acbf98f4128
unrealce <z35305515@gmail.com>
2017-01-15 18:03:41 +0800

    框架&组件

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
-//app.js
 App({
-  onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
+  /**
+   * 生命周期函数--监听小程序初始化
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: () => {
+    const logs = wx.getStorageSync('logs') || [] // 调用API从本地缓存中获取数据
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
+  /**
+   * 生命周期函数--监听小程序显示
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow : () => {
+    console.log('onShow')
+  },
+  /**
+   * 生命周期函数--监听小程序隐藏
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide : () => {
+    console.log('onHide')
+  },
+  /**
+   * 错误监听函数
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError : msg => {
+    console.error(`error: $msg`)
+  },
+  /**
+   * 其他函数
+   */
+  getUserInfo: function(cb) {
+    if (this.globalData.userInfo) {
+      typeof cb == 'function' && cb(this.globalData.userInfo)
+    } else {
+      wx.login({ // 调用登录接口
+        success: () => {
           wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
+            success: res => {
+              this.globalData.userInfo = res.userInfo
+              typeof cb == 'function' && cb(this.globalData.userInfo)
             }
           })
         }
       })
     }
   },
-  globalData:{
-    userInfo:null
-  }
+  /**
+   * 数据
+   */
+  globalData: {}
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,47 @@
-//index.js
-//获取应用实例
-var app = getApp()
+const app = getApp() // 获取应用实例
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
+  data: { // 页面的初始数据
+    motto: 'Hello World'
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onLoad: function() { // 生命周期函数--监听页面加载
+    console.log('onLoad')
+    app.getUserInfo(userInfo => { // 调用应用实例的方法获取全局数据
+      this.setData({ // 更新数据
+        userInfo: userInfo
+      })
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  onReady: () => { // 生命周期函数--监听页面初次渲染完成
+    console.log('onReady')
+  },
+  onShow: () => { // 生命周期函数--监听页面显示
+    console.log('onShow')
+  },
+  onHide: () => { // 生命周期函数--监听页面隐藏
+    console.log('onHide')
+  },
+  onUnload: () => { // 生命周期函数--监听页面卸载
+    console.log('onUnload')
+  },
+  onPullDownRefresh: () => { // 页面相关事件处理函数--监听用户下拉动作
+    console.log('onPullDownRefresh')
+  },
+  onReachBottom: () => { // 页面上拉触底事件的处理函数
+    console.log('onReachBottom')
+  },
+  onShareAppMessage: () => { // 用户点击右上角分享
+    console.log('onShareAppMessage')
+    return {
+      title: '自定义分享标题',
+      desc: '自定义分享描述',
+      path: 'pages/index/index'
+    }
+  },
+  // 开发者可以添加任意的函数或数据到 object 参数中，在页面的函数中用 this 可以访问
+  bindViewTap: function(event) {
+    console.log(`bindViewTap: ${event}`)
+    this.setData({ // 更新数据
+      motto: `Hello ${this.data.userInfo.nickName}`
     })
   }
 })
